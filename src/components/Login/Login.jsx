0f48ba5b86f6c9f263e88a915fe0952e5f875e03
Samuel <samuelcvmoura@gmail.com>
2022-10-10 20:26:22 -0300

    login bg and sign up page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,20 +2,23 @@ import React, { useContext } from "react"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { UserContext } from "../../UserContext"
 import { LoginForm, LoginCreate, LoginPwlost, LoginPwreset } from "./index"
+import styles from "./Login.module.css"
 
 const Login = () => {
   const { login } = useContext(UserContext)
 
   if (login === true) return <Navigate to="/account" />
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="create" element={<LoginCreate />} />
-        <Route path="lost" element={<LoginPwlost />} />
-        <Route path="reset" element={<LoginPwreset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="create" element={<LoginCreate />} />
+          <Route path="lost" element={<LoginPwlost />} />
+          <Route path="reset" element={<LoginPwreset />} />
+        </Routes>
+      </div>
+    </section>
   )
 }
 
--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -1,7 +1,24 @@
 import React from "react"
+import Input from "../FormComponents/Inputs/Input"
+import Button from "../FormComponents/Button/Button"
+import useForm from "../Hooks/useForm"
 
 const LoginCreate = () => {
-  return <div>LoginCreate</div>
+  const username = useForm()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+  }
+
+  return (
+    <section className="animateLeft">
+      <h1 className="title">Create your account!</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="User" type="text" name="username" />
+        <Button>Sign Up</Button>
+      </form>
+    </section>
+  )
 }
 
 export default LoginCreate
