84d58c1b194567ba843f8cc75ad6525c14b34fca
Samuel <samuelcvmoura@gmail.com>
2022-10-14 18:15:44 -0300

    styling comments form

--- src/components/Photos/PhotoCommentsForm.jsx
+++ src/components/Photos/PhotoCommentsForm.jsx
@@ -3,6 +3,7 @@ import { COMMENT_POST } from "../../api"
 import useFetch from "../Hooks/useFetch"
 import Error from "../Helper/Error"
 import { ReactComponent as Send } from "../../assets/enviar.svg"
+import styles from "./PhotoCommentsForm.module.css"
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = useState("")
@@ -19,15 +20,16 @@ const PhotoCommentsForm = ({ id, setComments }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comment here..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Send />
       </button>
       <Error error={error} />
